03af106a24315a5a1111701bb3c6915bdb86571f
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2023-08-08 17:00:17 +0530

    fixed websocket warning

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
+const nodeExternals = require("webpack-node-externals");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
 };
 
 module.exports = nextConfig;
+
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push({
+        bufferutil: "bufferutil",
+        "utf-8-validate": "utf-8-validate",
+        "supports-color": "supports-color",
+      });
+    }
+
+    return config;
+  },
+};
