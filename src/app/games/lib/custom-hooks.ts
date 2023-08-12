3746ee2e610f07f6dbadd36892c22d68be76840b
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2023-08-12 13:47:13 +0530

    Added use Swipe hook

--- src/app/games/lib/custom-hooks.ts
+++ src/app/games/lib/custom-hooks.ts
@@ -176,3 +176,31 @@ export function useGame(
     onAttempt,
   };
 }
+
+export function useSwipe() {
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+  const [swipeDirection, setSwipeDirection] = useState<
+    "left" | "right" | undefined
+  >();
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e: any) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e: any) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    // const isRightSwipe = distance < -minSwipeDistance;
+    setSwipeDirection(isLeftSwipe ? "left" : "right");
+  };
+
+  return { onTouchEnd, onTouchMove, onTouchStart, swipeDirection };
+}
