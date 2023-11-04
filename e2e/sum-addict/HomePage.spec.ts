3c33919a52071e49b29c6e1a7bfaa72437d23c09
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2023-11-04 23:38:17 +0530

    Wrote test for solo game

--- e2e/sum-addict/HomePage.spec.ts
+++ e2e/sum-addict/HomePage.spec.ts
@@ -27,54 +27,40 @@ test.describe("Home URL", () => {
 
   test("verify solo page", async ({ page }) => {
     await page.getByLabel("Play sum addiction").click();
+
+    // Click on the solo play button
     await page.getByRole("link", { name: "Solo" }).click();
-    await expect(page).toHaveURL(/games\/sum-addict\/play/);
-    await page.waitForTimeout(3500);
+
+    // Wait for game count down timer
+    await page.waitForTimeout(4500);
     //Verify that desired text is visible
     //await expect(page.getByText("Starting game")).toBeVisible();
+
+    // test if page is redirected to play page
+    await expect(page).toHaveURL(/games\/sum-addict\/play/);
+
+    // test if questin is visible
+    await expect(page.getByLabel("question")).toBeVisible();
     const question = await page.getByLabel("question").innerText();
-    // question = 8
-    console.log("Question = ", question);
+
+    // test if score is visible
+    await expect(page.getByLabel("score")).toBeVisible();
+    let score = await page.getByLabel("score").innerText();
+
     // Create random set of answers
     let answers = [getRandomInt(9), getRandomInt(9), getRandomInt(9)];
-    // answers = [1, 2, 3];
     let ansSum = answers.reduce((item, currentSum) => item + currentSum, 0);
-    // ansSum = 6;
-    //check if answers have unique values
-    let uniqueAnswers = new Set(answers);
-
-    while (
-      ansSum !== Number(question) &&
-      uniqueAnswers.size === answers.length - 1
-    ) {
+    while (ansSum !== Number(question)) {
       answers = [getRandomInt(9), getRandomInt(9), getRandomInt(9)];
       ansSum = answers.reduce((item, currentSum) => item + currentSum, 0);
-      uniqueAnswers = new Set(answers);
     }
-
-    console.log(
-      "Random set of answers",
-      uniqueAnswers,
-      answers,
-      uniqueAnswers.size,
-      answers.length
-    );
 
     // Loop the ansers array and click on that button
     for (const answer of answers) {
-      await page.getByText(`${answer}`, { exact: true }).click();
+      await page.locator(`[aria-label="option"] >> text="${answer}"`).click();
     }
-    //const score = await page.getByLabel("Score").innerText();
-    let score = await page
-      .locator("div")
-      .filter({ hasText: /^0$/ })
-      .nth(1)
-      .innerText();
-    console.log("Score = ", score);
-    expect(score).toBe("5");
-    // const score = page.getByAltText("Score icon").innerText();
-    //console.log("Score = ", score);
-
     // check the score is incremented by 5
+    score = await page.getByLabel("Score").innerText();
+    expect(score).toBe("5");
   });
 });
