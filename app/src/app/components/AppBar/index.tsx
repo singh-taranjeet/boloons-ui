929a587501454fb5d402cd328fae2eb952dca22c
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2024-02-27 16:18:25 -0800

    Deployment to sam (#10)
    * added-sam-configuration
    
    * configured-sam-pipeline
    
    ---------
    
    Co-authored-by: Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>

---
+++ app/src/app/components/AppBar/index.tsx
@@ -0,0 +1,56 @@
+import { Href } from "../Href";
+import Image from "next/image";
+import { flexCenter } from "@/app/lib/style.lib";
+import { urls } from "@/app/lib/constants.lib";
+
+interface NavItemProps {
+  href: string;
+  image: string;
+  title: string;
+  theme?: "dark" | "light";
+}
+const NavItem = (props: NavItemProps) => {
+  const { href, image, title, theme = "light" } = props;
+  return (
+    <li className={`${flexCenter}`}>
+      <Href
+        href={href}
+        color={theme === "light" ? "text-primary" : "text-white"}
+        bgColor={theme === "light" ? "bg-white" : "bg-primary"}
+        border="border-0"
+      >
+        <span className="flex">
+          <Image
+            src={image}
+            alt="boloons"
+            width={30}
+            height={30}
+            className="mr-small"
+          />
+          {title}
+        </span>
+      </Href>
+    </li>
+  );
+};
+
+export const AppBar = () => {
+  return (
+    <nav className="bg-primary bg-opacity-10 z-10 w-full fixed top-0">
+      <ul className="flex gap-normal justify-start p-normal flex-row">
+        <NavItem
+          theme="dark"
+          href={"/"}
+          image={`${urls.media}boloons-logo-small.webp`}
+          title={"Boloons"}
+        ></NavItem>
+
+        <NavItem
+          href={"/games"}
+          image={`${urls.media}games-logo-small.webp`}
+          title={"Games"}
+        ></NavItem>
+      </ul>
+    </nav>
+  );
+};
---
+++ app/src/app/games/sum-addict/components/BoxGrid/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { Box } from "../Box";
+import Image from "next/image";
+import { Number } from "../Number";
+import { urls } from "@/app/lib/constants.lib";
+import { FingerPointer } from "@/app/components/FingerPointer";
+
+interface BoxGridType {
+  options: number[];
+  onAttempt(attempt: number): void;
+  attempts: number[];
+  learningMode: boolean;
+}
+
+export function BoxGrid(props: Readonly<BoxGridType>) {
+  const { options, onAttempt, attempts = [], learningMode = false } = props;
+
+  return (
+    <section
+      className={`grid gap-normal justify-center bg-opacity-60 bg-pink-700 w-fit p-normal rounded mx-auto grid-cols-3`}
+    >
+      {options?.map((option) => (
+        <div key={option} className="relative">
+          <Box
+            selected={attempts.includes(option)}
+            onAttempt={() => onAttempt(option)}
+          >
+            <Number
+              label="option"
+              className={`text-very-large md:text-extra-very-large`}
+              number={option}
+              color="blue"
+            />
+          </Box>
+          {attempts.includes(option) && learningMode ? (
+            <FingerPointer className="bottom-0 -right-6" />
+          ) : null}
+        </div>
+      ))}
+    </section>
+  );
+}
