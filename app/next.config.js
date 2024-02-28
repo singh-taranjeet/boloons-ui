c72048da2ead6cc31ae7ee8cf2ad1496952c6459
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2024-02-27 17:26:48 -0800

    converted-to-pwa-app (#12)
    * converted-to-pwa-app
    
    * Update layout.tsx
    
    ---------
    
    Co-authored-by: Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>

--- app/next.config.js
+++ app/next.config.js
@@ -5,6 +5,10 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  swcMinify: true, // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
   async rewrites() {
     return [
       {
@@ -27,5 +31,13 @@ const nextConfig = {
   },
 };
 
+// Configuration object tells the next-pwa plugin
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
 // module.exports = withBundleAnalyzer(nextConfig);
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
