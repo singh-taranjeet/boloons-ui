82789c13c58c78b40a025ce4ca186d922a8bba1b
Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>
2024-03-01 18:53:11 +1100

    fix to allow http requests

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -27,6 +27,7 @@ export const metadata: Metadata = {
     description: AppConstants.metaData.description,
     images: "",
   },
+
   authors: { name: "Taranjeet Singh" },
 };
 
@@ -37,6 +38,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body className={`${inter.className} relative mx-auto h-screen`}>
         <AppBar />
         <Image
