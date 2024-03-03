884409bc1f0fe4edf573a41f7687d256ab92f9cd
Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>
2024-03-04 10:00:16 +1100

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
+  images: {
+    unoptimized: true,
+  },
   async rewrites() {
     return [
       {
