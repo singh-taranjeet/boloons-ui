0e9fe57400c41e6e2cb5e278dca23a6b19e29db5
Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>
2024-03-08 17:19:00 +1100

    removed-playwright

--- app/src/app/components/PageLoader/index.tsx
+++ app/src/app/components/PageLoader/index.tsx
@@ -1,12 +1,37 @@
-import { PulseLoading } from "../PulseLoading";
+const Pulse = () => {
+  return (
+    <section
+      className={`flex-center p-normal rounded-lg`}
+      role="alert"
+      aria-label="loading"
+      aria-busy={true}
+    >
+      <div className="p-4 max-w-sm w-full mx-auto">
+        <div className="animate-pulse flex space-x-4">
+          {/* <div className="rounded-full bg-slate-200 h-10 w-10"></div> */}
+          <div className="flex-1 space-y-6 py-1">
+            <div className="h-2 bg-primary rounded"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="h-2 bg-primary rounded col-span-2"></div>
+                <div className="h-2 bg-primary rounded col-span-1"></div>
+              </div>
+              <div className="h-2 bg-primary rounded"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export const PageLoader = () => {
   return (
     <section className="relative mt-48 w-full mx-normal flex flex-col">
-      <PulseLoading></PulseLoading>
-      <PulseLoading></PulseLoading>
-      <PulseLoading></PulseLoading>
-      <PulseLoading></PulseLoading>
+      <Pulse></Pulse>
+      <Pulse></Pulse>
+      <Pulse></Pulse>
+      <Pulse></Pulse>
     </section>
   );
 };
