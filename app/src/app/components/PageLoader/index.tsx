4086da13815922c45ea768981b6aeb77d516ab47
Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>
2024-03-08 11:54:04 +1100

    Update index.tsx

--- app/src/app/components/PageLoader/index.tsx
+++ app/src/app/components/PageLoader/index.tsx
@@ -2,7 +2,7 @@ import { PulseLoading } from "../PulseLoading";
 
 export const PageLoader = () => {
   return (
-    <section className="relative mt-48 w-full mx-normal flex flex-wrap">
+    <section className="relative mt-48 w-full mx-normal flex flex-col">
       <PulseLoading></PulseLoading>
       <PulseLoading></PulseLoading>
       <PulseLoading></PulseLoading>
