5824079c13df5f2e3881f9b3c5710c10735f84ee
Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>
2024-03-08 18:18:53 +1100

    added-playwright-sum-addict-e2e-tests

---
+++ app/e2e/games/games.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+test("Landing page: Game", async ({ page }) => {
+  await page.goto("https://www.boloons.com/games");
+
+  await page.getByRole("heading", { name: "Games", exact: true });
+  await page.getByRole("heading", { name: "Play games to train your brain" });
+  await page.getByRole("link", { name: "Play Sum Addict" });
+  await page.getByRole("link", { name: "Play Shapr" });
+  await page.getByRole("img", { name: "Sum Addict" });
+  await page.getByRole("img", { name: "Sharp" });
+  await page.getByRole("img", { name: "game-controller" });
+});
