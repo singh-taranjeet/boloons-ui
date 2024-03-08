e25209a8e68de2ec983b1570c15361c4f49e138d
Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>
2024-03-08 13:14:20 +1100

    added-home-page-test

--- app/unit-tests/app/index.test.tsx
+++ app/unit-tests/app/index.test.tsx
@@ -1,14 +1,15 @@
 import { screen, render } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import Page from "./page";
+import Page from "../../src/app/page";
 
 describe("Home page", () => {
   it("renders the home page", () => {
     render(<Page />);
+
     expect(screen.getByRole("heading", { name: /Boloons/i }));
 
-    expect(screen.getByRole("img", { name: /space-craft/i }));
+    expect(screen.getByRole("img", { name: /brain/i }));
 
-    expect(screen.getByRole("link", { name: /Play sum addiction/i }));
+    expect(screen.getByRole("img", { name: /get started/i }));
   });
 });
